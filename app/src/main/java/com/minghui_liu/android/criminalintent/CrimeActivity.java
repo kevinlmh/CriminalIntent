e5fd58bca465b43a7daf9e190244921cd5521f9c
kevin <minghui.liu@trincoll.edu>
2015-10-10 14:18:35 -0400

    Direct retrival of intent extra let CrimeFragment retrieve crimeId extra of CrimeActivity directly

--- app/src/main/java/com/minghui_liu/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/minghui_liu/android/criminalintent/CrimeActivity.java
@@ -1,11 +1,22 @@
 package com.minghui_liu.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+    public static final String EXTRA_CRIME_ID = "com.minghui-liu.com.android.criminalintent.crime_id";
+
+    public static Intent newIntent(Context packageContext, UUID crimeID) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeID);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment() {
