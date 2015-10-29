3bc7f7a72307fb5340e4307172cec0363ce35384
kevin <minghui.liu@trincoll.edu>
2015-10-28 21:59:09 -0400

    Ch 14 SQLite Database

--- app/src/main/java/com/minghui_liu/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/minghui_liu/android/criminalintent/CrimeListFragment.java
@@ -114,6 +114,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyItemChanged(mPosition);
         }
         updatSubtitle();
@@ -171,6 +172,10 @@ public class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
         }
     }
 }
