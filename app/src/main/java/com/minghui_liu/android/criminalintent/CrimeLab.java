c52a877e31d42a513bfade8ffccf91ced6ec4781
kevin <minghui.liu@trincoll.edu>
2015-10-29 11:02:49 -0400

    Challenge: Deleting Crimes deleteCrime(Crime) on CrimeLab, which uses mDatabase.delete(...)

--- app/src/main/java/com/minghui_liu/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/minghui_liu/android/criminalintent/CrimeLab.java
@@ -42,7 +42,9 @@ public class CrimeLab {
     }
 
     public void deleteCrime(Crime c) {
+        String uuidString = c.getId().toString();
 
+        mDatabase.delete(CrimeTable.NAME, CrimeTable.Cols.UUID + " = ?", new String[]{uuidString});
     }
 
     public List<Crime> getCrimes() {
